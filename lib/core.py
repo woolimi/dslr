cc85e186ebfd87281de49445adb9431ee8aa9532
namkim <namkim@student.42.fr>
2024-03-20 17:07:22 +0100

    feat: Histogram

--- lib/core.py
+++ lib/core.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def get_valid_values(values: pd.Series) -> pd.Series:
     """
@@ -6,3 +8,29 @@ def get_valid_values(values: pd.Series) -> pd.Series:
     """
     return values[values == values]
 
+
+def describe(filename):
+    df = pd.read_csv(filename, index_col="Index")
+
+# def get_columns(
+# draw_histogram
+    # which Hogwarts course has a homogenous score "distribution" between all four houses
+# draw_scatter_plot
+    # what are the two features that are similar
+# draw_pair_plot
+    # pair plot or scatter plot matrix
+
+    # from the visualization, what features are you going to use for your logistic regression?
+
+# Hogwarts House <- constantize?
+def get_dataframe(filename):
+    df = pd.read_csv(filename, index_col="Index")
+    return df
+
+def get_numeric_columns(dataframe):
+    return dataframe.select_dtypes('number').columns
+
+def draw_histogram(df, cols):
+    for column in cols:
+        sns.histplot(data=df, x=column, hue="Hogwarts House")
+        plt.show()
