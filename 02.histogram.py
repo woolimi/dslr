09bf1e2de94838047c6d1c1d6a16df8f6fdee41a
namkim <namkim@student.42.fr>
2024-03-29 22:05:31 +0100

    update 02-04

--- 02.histogram.py
+++ 02.histogram.py
@@ -1,6 +1,6 @@
 import argparse
 import os
-from lib.visualize import draw_histogram, get_dataframe, get_numeric_columns
+from lib.visualize import Visualization
 from lib.validate import is_csv
 
 
@@ -8,10 +8,16 @@ def main():
     parser = argparse.ArgumentParser(description="describe: a simple script to describe csv dataset")
     parser.add_argument("csv_file", type=is_csv, help="Path to the .csv file")
     args = parser.parse_args()
+    v = Visualization()
    
-    df = get_dataframe(args.csv_file)
-    cols = get_numeric_columns(df)
-    draw_histogram(df, cols)
+    v.set_dataframe(args.csv_file)
+    v.preprocess_data()
+    v.set_numeric_columns()
+    v.draw_histograms()
+    # v.distribution_by_house('Arithmancy')
+    dist1, dist2 = v.save_distributions()
+    print(dist1)
+    print(dist2)
 
 if __name__ == "__main__":
     main()
--- 04.pair_plot.py
+++ 04.pair_plot.py
@@ -1,7 +1,5 @@
 import argparse
-from lib.core import draw_pair_plot
-from lib.core import get_dataframe
-from lib.core import get_numeric_columns
+from lib.visualize import Visualization
 from lib.validate import is_csv
 
 
@@ -9,10 +7,12 @@ def main():
     parser = argparse.ArgumentParser(description="describe: a simple script to describe csv dataset")
     parser.add_argument("csv_file", type=is_csv, help="Path to the .csv file")
     args = parser.parse_args()
-   
-    df = get_dataframe(args.csv_file)
-    cols = get_numeric_columns(df)
-    draw_pair_plot(df, cols)
+    v = Visualization()
+
+    v.set_dataframe(args.csv_file)
+    v.preprocess_data()
+    v.set_numeric_columns()
+    v.draw_pair_plot()
 
 if __name__ == "__main__":
     main()
